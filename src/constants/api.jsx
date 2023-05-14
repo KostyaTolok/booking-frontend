4f9246725356c90d56b99d427a3ad9f79a53fd29
KostyaTolok <tolok.kostya@gmail.com>
2023-05-15 00:18:17 +0300

    fix ip address

--- src/constants/api.jsx
+++ src/constants/api.jsx
@@ -1,4 +1,4 @@
-const IP_ADDRESS = "localhost:80";
+const IP_ADDRESS = "34.118.97.246";
 
 export const API_BASE_URL = `http://${IP_ADDRESS}`;
 export const SEARCH_API_BASE_URL = `${API_BASE_URL}/search/api`;
