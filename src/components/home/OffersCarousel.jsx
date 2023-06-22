6123220e9daaeb8a34fc0653e2942d0474f4cab2
KostyaTolok <tolok.kostya@gmail.com>
2023-06-22 22:42:04 +0300

    add offers link

--- src/components/home/OffersCarousel.jsx
+++ src/components/home/OffersCarousel.jsx
@@ -2,20 +2,25 @@ import Carousel from "react-material-ui-carousel";
 import OffersCarouselItem from "./OffersCarouselItem";
 import minskOffer from "images/offer_1.jpg";
 import brestOffer from "images/offer_2.jpg";
+import { HOTELS_LIST_LINK } from "constants/links";
 
 function OffersCarousel() {
   let offers = [
     {
       name: "Explore the capital of Belarus",
       description: "Search deals on hotels, homes, and much more in Minsk",
-      url: "/",
+      url: HOTELS_LIST_LINK,
       image: minskOffer,
+      destination: "Minsk",
+      destinationId: 1,
     },
     {
       name: "Explore one of the oldest cities in Belarus",
       description: "Find a place to stay in Brest",
-      url: "/",
+      url: HOTELS_LIST_LINK,
       image: brestOffer,
+      destination: "Brest",
+      destinationId: 2,
     },
   ];
 
--- src/components/home/OffersCarouselItem.jsx
+++ src/components/home/OffersCarouselItem.jsx
@@ -1,18 +1,35 @@
 import { CAROUSEL_BACKGROUND_GRADIENT } from "constants/colors";
 import "./OffersCarouselItem.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { setDateRange, setDestination } from "redux/actions/hotelsListActions";
+import { useDispatch } from "react-redux";
 
 function OffersCarouselItem(props) {
+  const item = props.item;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  function onItemClick(event) {
+    event.preventDefault();
+    dispatch(setDestination({ destinationId: item.destinationId, destination: item.destination }));
+    let startDate = new Date();
+    let endDate = new Date();
+    endDate.setDate(startDate.getDate() + 7);
+    dispatch(setDateRange(startDate.toDateString(), endDate.toDateString()));
+    navigate(item.url);
+  }
+
   return (
-    <Link to={props.url}>
+    <Link to={item.url} onClick={(event) => onItemClick(event)}>
       <div
         className="offers-carousel-item"
         style={{
-          backgroundImage: `${CAROUSEL_BACKGROUND_GRADIENT}, url(${props.item.image})`,
+          backgroundImage: `${CAROUSEL_BACKGROUND_GRADIENT}, url(${item.image})`,
         }}
+        onClick={onItemClick}
       >
-        <h1 className="offers-carousel-item__title">{props.item.name}</h1>
-        <p className="offers-carousel-item__description">{props.item.description}</p>
+        <h1 className="offers-carousel-item__title">{item.name}</h1>
+        <p className="offers-carousel-item__description">{item.description}</p>
       </div>
     </Link>
   );
